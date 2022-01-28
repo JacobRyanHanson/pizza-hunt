858beb1c70eef2ae605efee77568c54d7b8e3e2a
JacobRyanHanson <jacobryanhanson@gmail.com>
2022-01-28 11:11:19 -0600

    Reformat spacing

--- public/assets/js/idb.js
+++ public/assets/js/idb.js
@@ -3,51 +3,50 @@ let db;
 const request = indexedDB.open('pizza_hunt', 1);
 
 request.onupgradeneeded = function (event) {
-    // save a reference to the database 
+    // Saves a reference to the database .
     const db = event.target.result;
-    // create an object store (table) called `new_pizza`, set it to have an auto incrementing primary key of sorts 
+    // Creates an object store (table) called `new_pizza`, set it to have an auto incrementing primary key of sorts. 
     db.createObjectStore('new_pizza', { autoIncrement: true });
 };
 
-// upon a successful 
 request.onsuccess = function (event) {
-    // when db is successfully created with its object store (from onupgradedneeded event above) or simply established a connection, save reference to db in global variable
+    // When db is successfully created with its object store (from onupgradedneeded event above) or simply established 
+    // a connection, save reference to db in global variable.
     db = event.target.result;
 
-    // check if app is online, if yes run uploadPizza() function to send all local db data to api
+    // Checks if app is online, if yes run uploadPizza() function to send all local db data to api.
     if (navigator.onLine) {
         uploadPizza();
     }
 };
 
 request.onerror = function (event) {
-    // log error here
     console.log(event.target.errorCode);
 };
 
 function saveRecord(record) {
-    // open a new transaction with the database with read and write permissions 
+    // Opens a new transaction with the database with read and write permissions. 
     const transaction = db.transaction(['new_pizza'], 'readwrite');
 
-    // access the object store for `new_pizza`
+    // Access the object store for `new_pizza`.
     const pizzaObjectStore = transaction.objectStore('new_pizza');
 
-    // add record to your store with add method
+    // Add record to your store with add method.
     pizzaObjectStore.add(record);
 }
 
 function uploadPizza() {
-    // open a transaction on your db
+    // Opens a transaction on your db.
     const transaction = db.transaction(['new_pizza'], 'readwrite');
 
-    // access your object store
+    // Access your object store.
     const pizzaObjectStore = transaction.objectStore('new_pizza');
 
-    // get all records from store and set to a variable
+    // Gets all records from store and set to a variable.
     const getAll = pizzaObjectStore.getAll();
 
     getAll.onsuccess = function () {
-        // if there was data in indexedDb's store, let's send it to the api server
+        // If there was data in indexedDb's store, let's send it to the api server.
         if (getAll.result.length > 0) {
             fetch('/api/pizzas', {
                 method: 'POST',
@@ -62,11 +61,11 @@ function uploadPizza() {
                     if (serverResponse.message) {
                         throw new Error(serverResponse);
                     }
-                    // open one more transaction
+                    // Open one more transaction.
                     const transaction = db.transaction(['new_pizza'], 'readwrite');
-                    // access the new_pizza object store
+                    // Access the new_pizza object store.
                     const pizzaObjectStore = transaction.objectStore('new_pizza');
-                    // clear all items in your store
+                    // Clears all items in your store.
                     pizzaObjectStore.clear();
 
                     alert('All saved pizza has been submitted!');
